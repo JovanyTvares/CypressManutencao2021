6a5c7e29563e0ccd7ebb456d300ca1ef57e00080
Jovany <jovany.tavares@projetusti.com.br>
2021-11-29 17:10:04 -0300

    Initial Commit with Cypress

---
+++ cypress/integration/enade_testes/EnadeTeste.spec.js
@@ -0,0 +1,32 @@
+/// <reference types = "cypress"/>
+
+describe('Enade Teste', () => {
+
+    describe('Teste Usuário', () => {
+        it('teste1', () => {
+        
+        });
+    });
+
+    describe('Enade Provas', () => {
+        it('teste2', () => {
+        
+        });
+    });
+
+    describe('Enade Questões', () => {
+        it('teste3', () => {
+        
+        });
+    });
+
+    describe('Enade Resultados', () => {
+        it('teste4', () => {
+        
+        });
+    });
+});
+
+
+
+
